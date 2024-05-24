820e0f25d3adb4b9a573497fa0728a392b863059
anyoneaaaa <agarwalayush19999@gmail.com>
2024-05-24 22:24:20 +0530

    update

--- ReduxToolkit/src/App.jsx
+++ ReduxToolkit/src/App.jsx
@@ -1,3 +1,5 @@
+import AddTodo from "./components/AddTodo"
+import Todos from "./components/Todos"
 
 
 function App() {
@@ -6,6 +8,8 @@ function App() {
   return (
     <>
      <h1>Redux toolkit Todo</h1>
+     <AddTodo/>
+     <Todos/>
     </>
   )
 }
